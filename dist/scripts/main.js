639c8f4b63d965596400ed207c894c1e751be9c0
edfer <eduardojfernandezb@gmail.com>
2018-03-14 21:25:29 +0100

    adaptando la interacción del usuario con las fotos para cada dispositivo

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -10258,8 +10258,19 @@ require('./saturate-desaturate');
 },{"./saturate-desaturate":3}],3:[function(require,module,exports){
 var $ = require('jquery');
 
-$('.board-item').on('click', function(){
-    $(this).find('.img').toggleClass('desaturate');
-    $(this).find('.hidden').toggleClass('description'); 
+$('.board-item').on('click ', function() {
+
+    var viewWidth = $(window).width();
+
+    if (viewWidth <= 1025) {
+        $(this).find('.img').toggleClass('desaturate');
+        $(this).find('.hidden').toggleClass('description');
+    }
+
+
+});
+
+$('.board-item').on('mouseover mouseout', function() {
+    $(this).find('.img').toggleClass('hover-color');
 });
 },{"jquery":1}]},{},[2]);
--- src/scripts/saturate-desaturate.js
+++ src/scripts/saturate-desaturate.js
@@ -1,6 +1,17 @@
 var $ = require('jquery');
 
-$('.board-item').on('click', function(){
-    $(this).find('.img').toggleClass('desaturate');
-    $(this).find('.hidden').toggleClass('description'); 
+$('.board-item').on('click ', function() {
+
+    var viewWidth = $(window).width();
+
+    if (viewWidth <= 1025) {
+        $(this).find('.img').toggleClass('desaturate');
+        $(this).find('.hidden').toggleClass('description');
+    }
+
+
+});
+
+$('.board-item').on('mouseover mouseout', function() {
+    $(this).find('.img').toggleClass('hover-color');
 });
